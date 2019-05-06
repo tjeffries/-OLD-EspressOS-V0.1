5880008bd21fea2caeacef753c89214dae497c19
tjeffries <tjeffries@users.noreply.github.com>
2019-05-05 20:02:59 -0500

    Add files via upload
    Removed MAX6675 functionality, renamed vars to clarify steam/brew boilers, added voltage testing framework for brew temperature/steam pressure/steam boiler water level sensors

--- EspressOS.ino
+++ EspressOS.ino
@@ -1,87 +1,63 @@
 /* Thomas Jeffries 2019 - thomasejeffries@gmail.com -
    Free to use and share under MIT license (see end of file for license blurb) */
 
-/*  Max6675 #1 (brew) ==>   Arduino
-      SCK             ==>     D6
-      CS              ==>     D7
-      SO              ==>     D8
+/*  Thermister (brew) ==>     A0
+    Pressure (steam)  ==>     A1
+    Steam water Level ==>     A2   */
+
+/* I2C LCD Module
+      Gnd             ==>     Gnd
       Vcc             ==>     Vcc (5v)
-      Gnd             ==>     Gnd      */
-
-/*   SSR #1 (brew)    ==>     D5   */
-
-/*  Max6675 #2 (steam)==>   Arduino
-      SCK             ==>     D10
-      CS              ==>     D11
-      SO              ==>     D12
-      Vcc             ==>     Vcc (5v)
-      Gnd             ==>     Gnd      */
-
-/*   SSR #2 (steam)   ==>     D9   */
-
-/*    i2c LCD Module  ==>   Arduino
-      SCL             ==>     A5
       SDA             ==>     A4
-      Vcc             ==>     Vcc (5v)
-      Gnd             ==>     Gnd      */
+      SCL             ==>     A5      */
 
-/*    Rotary Encoder  ==>   Arduino
+/* Rotary Encoder
       SW              ==>     D2
       DTA             ==>     D3
       CLK             ==>     D4
       Vcc             ==>     Vcc (5v)
       Gnd             ==>     Gnd      */
 
-/*  Pump switch down  ==>    D13
-    Pump switch up    ==>    A0
-    brew solenoid     ==>    A1
-    steam solenoid    ==>    A2
-    pump relay        ==>    A3 */
+/*   SSR #1 (brew)    ==>     D5   */
+/*   SSR #2 (steam)   ==>     D6   */
+
+/*  Pump switch down  ==>     D7
+    Pump switch up    ==>     D8
+    brew solenoid     ==>     D9
+    steam solenoid    ==>     D10
+    pump relay        ==>     D11 */
 
 #include <Wire.h>
-#include <MAX6675_Thermocouple.h>
 #include <LiquidCrystal_I2C.h>
 LiquidCrystal_I2C lcd(0x27, 16, 2); //usually 0x27 for 16x2; use I2C scanner to determine address
-MAX6675_Thermocouple* thermo1;
-MAX6675_Thermocouple* thermo2;
 
 //I/O pin definitions:
 #define ROTA_SW    2  //rotary encoder push switch
 #define ROTA_DT    3  //rotary encoder data line
 #define ROTA_CLK   4  //rotary encoder clock line
-#define BOILER_1   5  //Pin for signal to SSR #1 (brew)
-#define BOILER_2   9  //Pin for signal to SSR #2 (steam)
-//MAX6675 #1 (brew temp) SPI pins
-#define TEMP1_SCK  6
-#define TEMP1_CS   7
-#define TEMP1_SO   8
-//MAX6675 #2 (steam temp) SPI pins
-#define TEMP2_SCK  10
-#define TEMP2_CS   11
-#define TEMP2_SO   12
-//switch input and relay outputs
-#define PUMP_SW_DN 13
-#define PUMP_SW_UP A0
-#define BREW_SOLENOID A1
-#define STEAM_SOLENOID A2
-#define PUMP_RELAY A3
+#define BREW_COIL  5  //Pin for signal to SSR #1 (brew)
+#define STEAM_COIL 6  //Pin for signal to SSR #2 (steam)
+//switches and relays
+#define PUMP_SW_DN     7
+#define PUMP_SW_UP     8
+#define PUMP_RELAY     9
+#define BREW_SOLENOID  10
+#define STEAM_SOLENOID 11
+//boiler sensor inputs
+#define BREW_TEMP    A0
+#define STEAM_PRESS  A1
+#define STEAM_LEVEL  A2
 
 //thermocouple constants
 const float thermoCal1 = -0.4;
-const float thermoCal2 = -1.0;
-const int TEMP_AVG_COUNT = 5;
 
 //Brew boiler (boiler #1) temperature variables
-float setTemp1 = 95; //Default brew temperature setpoint
-float curTemp1, prevTemp1, error1, prevError1, PIDValue1;
-uint8_t tempIndex1 = 0;
-float tempArray1[TEMP_AVG_COUNT];
+float setTemp = 95; //Default brew temperature setpoint (Celsius)
+float curTemp, prevTemp, tempErr, prevTempErr, PIDTemp;
 
 //Steam boiler (boiler #2) temperature variables
-float setTemp2 = 133; //Default steam temperature setpoint
-float curTemp2, prevTemp2, error2, prevError2, PIDValue2;
-uint8_t tempIndex2 = 0;
-float tempArray2[TEMP_AVG_COUNT];
+float setPress = 1.5; //Default steam pressure setpoint (Bar)
+float curPress, prevPress, pressErr, prevPressErr, PIDPress;
 
 //boiler elapsed time variables
 float curTime, prevTime;
@@ -98,7 +74,7 @@ float kp1 = 1.0, ki1 = 0.0, kd1 = -1.0;
 float kp2 = 2.0, ki2 = 0.0, kd2 = -4.0;
 
 //Boiler duty cycle settings (0-10, x10%)
-int boilerDuty1, boilerDuty2;
+int brewDuty, steamDuty;
 
 ///////////////////CONTROL/INPUT//////////////
 volatile int rotorValue;
@@ -121,8 +97,8 @@ void setup() {
   Serial.begin(9600);
 
   //setup IO pins
-  pinMode(BOILER_1, OUTPUT);
-  pinMode(BOILER_2, OUTPUT);
+  pinMode(BREW_COIL, OUTPUT);
+  pinMode(STEAM_COIL, OUTPUT);
   //set relay pins base state to high (off) to prevent flutter on startup
   pinMode(BREW_SOLENOID, INPUT_PULLUP);
   pinMode(STEAM_SOLENOID, INPUT_PULLUP);
@@ -134,11 +110,6 @@ void setup() {
   pinMode(PUMP_SW_UP, INPUT_PULLUP);//fill steam boiler
 
   curTime = millis();
-
-  //setup both MAX6675 modules:
-  thermo1 = new MAX6675_Thermocouple(TEMP1_SCK, TEMP1_CS, TEMP1_SO);
-  thermo2 = new MAX6675_Thermocouple(TEMP2_SCK, TEMP2_CS, TEMP2_SO);
-
 
   //register fast-access ports and corresponding masks
   swPinMask = digitalPinToBitMask(ROTA_SW);
@@ -159,8 +130,8 @@ void setup() {
   loopCount = 0;
   menuState = 0;
 
-  boilerDuty1 = 0;
-  boilerDuty2 = 0;
+  brewDuty = 0;
+  steamDuty = 0;
 
   lcd.init();
   lcd.backlight();
@@ -168,7 +139,7 @@ void setup() {
 
 
 void loop() {
-  //update menu state base on button click count
+  //update menu state based on button click count
   menuState += buttonCount;
   buttonCount = 0;
 
@@ -179,27 +150,18 @@ void loop() {
 
   //update display every 500ms
   if ((loopCount % 5) == 0) {
-    //subtract oldest sample from average
-    curTemp1 -= tempArray1[tempIndex1];
-    curTemp2 -= tempArray2[tempIndex2];
-    //save temp samples in averaging array
-    tempArray1[tempIndex1] = thermo1->readCelsius() + thermoCal1;
-    tempArray2[tempIndex1] = thermo2->readCelsius() + thermoCal2;
-    //add newest sample to average
-    curTemp1 += tempArray1[tempIndex1];
-    curTemp2 += tempArray2[tempIndex2];
-    //calculate new temp array indices
-    tempIndex1 = (++tempIndex1) % TEMP_AVG_COUNT;
-    tempIndex2 = (++tempIndex2) % TEMP_AVG_COUNT;
+
+    getBrewTemp();
+    getSteamPress();
 
     lcd.setCursor(0, 0);
-    lcd.print(curTemp1 / (TEMP_AVG_COUNT + 0.0), 2);
+    lcd.print(curTemp, 2);
     lcd.setCursor(8, 0);
-    lcd.print(curTemp2 / (TEMP_AVG_COUNT + 0.0), 2);
+    lcd.print(curPress, 2);
     lcd.setCursor(0, 1);
-    lcd.print(boilerDuty1);
+    lcd.print(brewDuty);
     lcd.setCursor(8, 1);
-    lcd.print(boilerDuty2);
+    lcd.print(steamDuty);
 
 //    char out[32];
 //    int curTrunk1 = curTemp1/TEMP_AVG_COUNT, curDecimal1 = (curTemp1*10-curTrunk1*10)/(10*TEMP_AVG_COUNT);
@@ -207,7 +169,6 @@ void loop() {
 //    sprintf(out, "brew: %d.%d; steam: %d.%d\n", curTrunk1, curDecimal1, curTrunk2, curDecimal2);
 //    Serial.print(out);
   }
-
 
   setBoilerStates();
   setSwitchStates();
@@ -222,21 +183,36 @@ void loop() {
   loopCount = (++loopCount) % 200;
 }
 
+
+void getBrewTemp(){
+  uint8_t brewVoltage = analogRead(BREW_TEMP);
+}
+
+
+void getSteamPress(){
+  uint8_t steamVoltage = analogRead(STEAM_PRESS);
+}
+
+
+void getWaterLevel(){
+  uint8_t levelVoltage = analogRead(STEAM_LEVEL);
+}
+
+
 const uint8_t CLOCK_MULTIPLE = 1;
 //duty cycle implementation method
 void setBoilerStates() {
-
-  if (loopCount % (10 * CLOCK_MULTIPLE) == 0) {
-    digitalWrite(BOILER_1, HIGH);
-    digitalWrite(BOILER_2, HIGH);
+  uint8_t slice = loopCount % (10 * CLOCK_MULTIPLE);
+  if (slice == 0) {
+    digitalWrite(BREW_COIL, HIGH);
+    digitalWrite(STEAM_COIL, LOW);
   }
 
-  if (loopCount % (10 * CLOCK_MULTIPLE) >= (boilerDuty1 * CLOCK_MULTIPLE)) {
-    digitalWrite(BOILER_1, LOW);
-  }
-
-  if (loopCount % (10 * CLOCK_MULTIPLE) >= (boilerDuty2 * CLOCK_MULTIPLE)) {
-    digitalWrite(BOILER_2, LOW);
+  if (slice >= (brewDuty * CLOCK_MULTIPLE)) {
+    digitalWrite(BREW_COIL, LOW);
+    if (slice >= ((10-steamDuty) * CLOCK_MULTIPLE)) { //nested to prevent combined 3kW load :P
+      digitalWrite(STEAM_COIL, HIGH);
+    }
   }
 }
 
@@ -261,47 +237,47 @@ void menu() {
         time2, timePrev2, PIDValue2;      */
 //performs PID calculation and sets boiler duty cycle globals (0-10, proportion of time on)
 void calculatePID() {
-  //  boilerDuty1 = 5;//SSR test routine
-  //  boilerDuty2 = 5;
-  //  return;
+    brewDuty = 5;//SSR test routine
+    steamDuty = 5;
+    return;
 
-  //perform PID math here
-  float p1, i1, d1, p2, i2, d2;
-
-  //setup PID vars
-  prevTime = curTime; curTime = millis();
-  error1 = setTemp1 - (curTemp1 / (TEMP_AVG_COUNT + 0.0));
-  error2 = setTemp2 - (curTemp2 / (TEMP_AVG_COUNT + 0.0));
-
-  //calculate P values
-  p1 = kp1 * error1;
-  p2 = kp2 * error2;
-
-  //calculate I values
-  i1 = ki1 * (error1 + prevError1) / 2.0 * ((curTime - prevTime) / 1000.0);
-  i2 = ki2 * (error2 + prevError2) / 2.0 * ((curTime - prevTime) / 1000.0);
-
-  //calculate D values
-  d1 = kd1 * ((error1 - prevError1) / ((curTime - prevTime) / 1000.0));
-  d2 = kd2 * ((error2 - prevError2) / ((curTime - prevTime) / 1000.0));
-
-  //reset vars for next cycle
-  prevTemp1 = curTemp1; prevError1 = error1;
-  prevTemp2 = curTemp2; prevError2 = error2;
-
-  //sum and set PID values
-  PIDValue1 = p1; // + i1 + d1;
-  PIDValue2 = p2; // + i2 + d2;
-
-  //  lcd.setCursor(8,1);
-  //  lcd.print(p1);
-  //  lcd.print(i1);
-  //  lcd.print(d1);
-  //lcd.print(p1+" "+i1+" "+d1);
-
-  //round (add .5 and truncate to int) to get duty cycle
-  boilerDuty1 = (PIDValue1 + 0.5);
-  boilerDuty2 = (PIDValue2 + 0.5);
+//  //perform PID math here
+//  float p1, i1, d1, p2, i2, d2;
+//
+//  //setup PID vars
+//  prevTime = curTime; curTime = millis();
+//  error1 = setTemp1 - (curTemp1 / (TEMP_AVG_COUNT + 0.0));
+//  error2 = setTemp2 - (curTemp2 / (TEMP_AVG_COUNT + 0.0));
+//
+//  //calculate P values
+//  p1 = kp1 * error1;
+//  p2 = kp2 * error2;
+//
+//  //calculate I values
+//  i1 = ki1 * (error1 + prevError1) / 2.0 * ((curTime - prevTime) / 1000.0);
+//  i2 = ki2 * (error2 + prevError2) / 2.0 * ((curTime - prevTime) / 1000.0);
+//
+//  //calculate D values
+//  d1 = kd1 * ((error1 - prevError1) / ((curTime - prevTime) / 1000.0));
+//  d2 = kd2 * ((error2 - prevError2) / ((curTime - prevTime) / 1000.0));
+//
+//  //reset vars for next cycle
+//  prevTemp1 = curTemp1; prevError1 = error1;
+//  prevTemp2 = curTemp2; prevError2 = error2;
+//
+//  //sum and set PID values
+//  PIDValue1 = p1; // + i1 + d1;
+//  PIDValue2 = p2; // + i2 + d2;
+//
+//  //  lcd.setCursor(8,1);
+//  //  lcd.print(p1);
+//  //  lcd.print(i1);
+//  //  lcd.print(d1);
+//  //lcd.print(p1+" "+i1+" "+d1);
+//
+//  //round (add .5 and truncate to int) to get duty cycle
+//  boilerDuty1 = (PIDValue1 + 0.5);
+//  boilerDuty2 = (PIDValue2 + 0.5);
 }
 
 
@@ -319,8 +295,6 @@ void rotateInterrupt() {
 void buttonInterrupt() {
   buttonCount++;
 }
-
-
 
 
 
